343d191d5e586e249b4052aac5b812a226c8043f
kobby08 <linusquam@gmail.com>
2022-08-07 18:29:59 +0000

    accessing context using context consumer

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -2,23 +2,34 @@ import { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Booklist extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The Gods must be crazy</li>
-          <li style={{ background: theme.ui }}>Harry Potter</li>
-          <li style={{ background: theme.ui }}>God of war</li>
-        </ul>
-      </div>
+      // we can access context using the Consumer property from the context api
+      // we use JSX to write a call back function that accepts the value of the context as a param
+
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+
+          return (
+            <div
+              className="book-list"
+              style={{ background: theme.bg, color: theme.syntax }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The Gods must be crazy</li>
+                <li style={{ background: theme.ui }}>Harry Potter</li>
+                <li style={{ background: theme.ui }}>God of war</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
+
+// this way of accessing context can be used in functional components too.
 
 export default Booklist;
